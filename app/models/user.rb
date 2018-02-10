a81748e5efaf6b0dfa1bc8cdd6d0b49d128c9333
Kasun <kasun.maldeni@gmail.com>
2018-02-10 17:24:22 +1100

    update generating user to include group id

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   belongs_to :group
 
   private
-  def self.generate_user email
-    User.create(email: email, uuid: SecureRandom.uuid, password: SecureRandom.hex, active: false)
+  def self.generate_user email, group_id
+    User.create(email: email, uuid: SecureRandom.uuid, password: SecureRandom.hex, active: false, group_id: group_id)
   end
 end
 
