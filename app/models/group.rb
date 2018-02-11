19597efd7e903280014e6cf32926dafbdb330860
Kasun <kasun.maldeni@gmail.com>
2018-02-12 00:47:19 +1100

    change create user to a public method in group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,6 @@ class Group < ApplicationRecord
   has_many :group_tasks
   has_many :tasks, through: :group_tasks
 
-  private
   def create_user email
     self.users.create(email: email, uuid: SecureRandom.uuid, password: SecureRandom.hex, active: false)
   end
