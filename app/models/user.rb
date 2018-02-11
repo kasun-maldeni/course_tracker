8c7209869be6172c61a773c3ca9bacd6c57b97e8
Kasun <kasun.maldeni@gmail.com>
2018-02-12 03:10:34 +1100

    user passwords must be at least 8 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :password, confirmation: true
+  validates :password, confirmation: true,
+                       length: {minimum: 8}
 
   belongs_to :group
 end
