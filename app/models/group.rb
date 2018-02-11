e7186f40b42159fe689f261d4c638b6fb34557a5
Kasun <kasun.maldeni@gmail.com>
2018-02-12 00:34:43 +1100

    move user creation to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,9 @@ class Group < ApplicationRecord
   has_many :users
   has_many :group_tasks
   has_many :tasks, through: :group_tasks
+
+  private
+  def create_user email
+    self.users.create(email: email, uuid: SecureRandom.uuid, password: SecureRandom.hex, active: false)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
   belongs_to :group
-
-  private
-  def self.generate_user email, group_id
-    User.create(email: email, uuid: SecureRandom.uuid, password: SecureRandom.hex, active: false, group_id: group_id)
-  end
 end
-
