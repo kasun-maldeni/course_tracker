723d2f850fe4338ccaeb79c65ebf72c1f7913004
Kasun <kasun.maldeni@gmail.com>
2018-02-18 15:19:52 +1100

    refactor checking github status code

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,20 +1,29 @@
-document.addEventListener('turbolinks:load', function(event) {
-  var ghUsername = document.querySelector('[data-gh-username]').dataset.ghUsername
-  var githubPRInfo = function(message) {
-    console.log(message);
-  };
-  githubPRInfo('Checking Github...')
-  var getPRs = fetch('https://api.github.com/repos/kasun-maldeni/WDI_14_HOMEWORK/pulls').then(function(response) {
-    return response.json();
-  }).then(function(pullRequests) {
-    return pullRequests.some(function(req) {
-      return req.user.login === ghUsername;
-    })
-  }).then(function(prFound) {
-    var message = prFound ? 'Homework submitted' : 'Waiting for submission';
-    githubPRInfo(message);
-  }).catch(function() {
-    var message = 'Failed to connect to Github';
-    githubPRInfo(message);
+(function(QS, QSA){
+  document.addEventListener('turbolinks:load', function(event) {
+    if (document.querySelector('.dashboard.index')) {
+      checkGithubForHomework();
+    }
   });
-});
+
+  function checkGithubForHomework() {
+    QS('.github-status').textContent = 'Checking Github...'
+    searchGithub()
+    setInterval(searchGithub, 15*1000);
+
+    function searchGithub() {
+      var getPRs = fetch('https://api.github.com/repos/kasun-maldeni/WDI_14_HOMEWORK/pulls').then(function(response) {
+        return response.json();
+      }).then(function(pullRequests) {
+        var ghUsername = QS('[data-gh-username]').dataset.ghUsername;
+        return pullRequests.some(function(req) {
+          return req.user.login === ghUsername;
+        })
+      }).then(function(prFound) {
+        var message = prFound ? 'Homework submitted' : 'Waiting for submission';
+        QS('.github-status').textContent = message;
+      }).catch(function() {
+        QS('.github-status').textContent = 'Failed to connect to Github';
+      });
+    }
+  }
+})(document.querySelector.bind(document), document.querySelectorAll.bind(document))
