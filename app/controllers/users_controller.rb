8e99d0949d535d040dc913e03000484f34912f2f
Kasun <kasun.maldeni@gmail.com>
2018-02-21 00:13:32 +1100

    refactor users controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:sign_up, :first_update]
+  before_action :user_already_signed_up, only: [:sign_up, :first_update]
 
   def sign_up
-    @user = User.find_by(uuid: params[:uuid])
-    redirect_to root_path if @user.signed_up
   end
 
   def first_update
-    @user = User.find_by(uuid: params[:uuid])
-    @user.signed_up = true
-    if @user.update(user_params)
+    if @user.update(first_update_params)
       session[:user_id] = @user.id
       flash[:success] = "You've signed up successfully!"
       redirect_to root_path
@@ -19,8 +16,12 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:email, :first_name, :last_name, :github_username, :password, :password_confirmation)
+  def first_update_params
+    params.require(:user).permit(:email, :first_name, :last_name, :github_username, :password, :password_confirmation).merge(signed_up: true)
+  end
+
+  def user_already_signed_up
+    @user = User.find_by(uuid: params[:uuid])
+    redirect_to root_path if @user.signed_up
   end
 end
-
