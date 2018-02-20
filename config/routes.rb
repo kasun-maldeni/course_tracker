8b9554715ef6d9a5111e7b3b8caeefded15d2751
Kasun <kasun.maldeni@gmail.com>
2018-02-21 00:19:04 +1100

    add dashboard views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   get '/warmups', to: 'dashboard#warmups', as: 'warmups'
   get '/homework', to: 'dashboard#homework', as: 'homework'
+  get '/task_progress/:user_group_task_id', to: 'dashboard#task_progress', as: 'task_progress'
 
   get '/sign_up/:uuid', to: 'users#sign_up'
   patch '/sign_up/:uuid', to: 'users#first_update'
