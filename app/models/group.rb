19e031094cff749975009afe9b1b108efc806aea
Kasun <kasun.maldeni@gmail.com>
2018-02-22 06:12:19 +1100

    add admin column to users table

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,6 @@ class Group < ApplicationRecord
   has_many :tasks, through: :group_tasks
 
   def create_user email
-    self.users.create(email: email, uuid: SecureRandom.uuid, password: SecureRandom.hex, signed_up: false)
+    self.users.create(email: email, uuid: SecureRandom.uuid, password: SecureRandom.hex, signed_up: false, admin: false)
   end
 end
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221185555) do
+ActiveRecord::Schema.define(version: 20180221190418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20180221185555) do
     t.datetime "updated_at", null: false
     t.bigint "group_id"
     t.boolean "signed_up"
+    t.boolean "admin"
     t.index ["group_id"], name: "index_users_on_group_id"
   end
 
