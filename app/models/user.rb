c995245906dd7ddc40e873f67f0b285971af6c6e
Kasun <kasun.maldeni@gmail.com>
2018-02-22 04:13:59 +1100

    update password validations to include a number and capital letter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ApplicationRecord
   belongs_to :group
   has_many :user_group_tasks
   has_many :group_tasks, through: :user_group_tasks
-  
+
   has_secure_password
   validates :email, :first_name, :last_name, :github_username, presence: true
   validates :password, confirmation: true,
-                       length: {minimum: 8}
+                       length: {minimum: 8},
+                       format: {
+                         with: /(?=.*[A-Z])(?=.*[\d])/,
+                         message: 'must include at least one number and capital letter'
+                       }
 end
