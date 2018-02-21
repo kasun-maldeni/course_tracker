b104ec9897f1c4669faa1624a49117603db7f746
Kasun <kasun.maldeni@gmail.com>
2018-02-22 02:29:44 +1100

    add user model validations to make all fields mandatory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates :password, confirmation: true,
-                       length: {minimum: 8}
-
   belongs_to :group
   has_many :user_group_tasks
   has_many :group_tasks, through: :user_group_tasks
+  
+  has_secure_password
+  validates :email, :first_name, :last_name, :github_username, presence: true
+  validates :password, confirmation: true,
+                       length: {minimum: 8}
 end
-
