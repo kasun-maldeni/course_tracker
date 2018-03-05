29c9457f7005f0e6bb59dab1ff9d03661df27364
Kasun <kasun.maldeni@gmail.com>
2018-03-05 11:50:05 +1100

    create roles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227170713) do
+ActiveRecord::Schema.define(version: 20180304141322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,10 @@ ActiveRecord::Schema.define(version: 20180227170713) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_feedbacks_on_author_id"
     t.index ["user_cohort_task_id"], name: "index_feedbacks_on_user_cohort_task_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "tasks", force: :cascade do |t|
