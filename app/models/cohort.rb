5473501cd1f9fa84c1430fdc36d729fc23a55377
Kasun <kasun.maldeni@gmail.com>
2018-03-15 00:11:38 +1100

    add methods to create users and cohorts

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,10 +1,13 @@
 class Cohort < ApplicationRecord
-  has_many :users
+  has_and_belongs_to_many :users
   has_many :cohort_tasks
   has_many :tasks, through: :cohort_tasks
 
+  def create_staff email
+    self.users.create_user 'staff', email
+  end
+
   def create_student email
-    role_id = Role.find_by(name: 'student')
-    self.users.create(email: email, uuid: SecureRandom.uuid, password: SecureRandom.hex, is_signed_up: false, is_active: true, role_id: role_id)
+    self.users.create_user 'student', email
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,27 @@
 class User < ApplicationRecord
-  belongs_to :cohort
   has_and_belongs_to_many :cohorts
+  belongs_to :role
 
   has_secure_password
-  validates :email, :first_name, :last_name, :github_username, presence: true
+  validates :email, :first_name, :last_name, :github_username, presence: true, on: :update
+  validates :email, uniqueness: {case_sensitive: false}
   validates :password, confirmation: true,
                        length: {minimum: 8},
                        format: {
                          with: /(?=.*[A-Z])(?=.*[\d])/,
                          message: 'must include at least one number and capital letter'
-                       }
+                       }, on: :update
+
+  def self.create_admin email
+    create_user 'admin', email
+  end
+
+  def create_cohort name
+    self.cohorts.create(name: name) if self.role.name == 'admin'
+  end
+
+  def self.create_user role_name, email
+    role_id = Role.find_by(name: role_name).id
+    User.create(email: email, uuid: SecureRandom.uuid, password: SecureRandom.hex, is_signed_up: false, is_active: true, role_id: role_id)
+  end
 end
