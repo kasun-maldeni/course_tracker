483cc0cf8855db9e5629787a9c49d066723ad13e
Kasun <kasun.maldeni@gmail.com>
2018-04-11 01:47:48 +1000

    show exact time warmup is being released for admins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def local_time(datetime)
     datetime.in_time_zone('Melbourne').strftime('%a, %d %b')
   end
+
+  def local_time_hours(datetime)
+    datetime.in_time_zone('Melbourne').strftime('%a, %d %b %H:%M')
+  end
 end
